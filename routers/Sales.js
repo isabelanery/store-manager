c8cbff3458452162de247873e8e7fa986abdfd32
isabelanery <isabelanery242@gmail.com>
2022-06-30 21:48:36 -0300

    feat: add route to GET /sales and GET /sales/:id

--- routers/Sales.js
+++ routers/Sales.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 const Sales = require('../controllers/Sales');
 
+router.route('/:id')
+  .get(Sales.findById);
+
 router.route('/')
+  .get(Sales.getAll)
   .post(Sales.create);
 
 module.exports = router;
